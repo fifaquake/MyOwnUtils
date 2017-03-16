8cb3ac5e0580c51935cd8b286a2e6717df0141e2
fifaquake_company <lipengweb@gmail.com>
2017-03-16 14:16:15 +0800

    Add comments for the EclipsePlugin.

--- EclipsePlugIn.bat
+++ EclipsePlugIn.bat
@@ -1,3 +1,5 @@
+REM Vim Wrapper
 eclipse -application org.eclipse.equinox.p2.director -repository http://vrapper.sourceforge.net/update-site/stable -installIU net.sourceforge.vrapper.feature.group
 
+REM color theme
 eclipse -application org.eclipse.equinox.p2.director -repository http://eclipse-color-theme.github.com/update -installIU com.github.eclipsecolortheme.feature.feature.group
