4225ab45e4d2823c502525605280c5c5875fa7a3
fifaquake_company <lipengweb@gmail.com>
2017-03-16 14:14:58 +0800

    Favorite Eclipse Plugin.

---
+++ EclipsePlugIn.bat
@@ -0,0 +1,3 @@
+eclipse -application org.eclipse.equinox.p2.director -repository http://vrapper.sourceforge.net/update-site/stable -installIU net.sourceforge.vrapper.feature.group
+
+eclipse -application org.eclipse.equinox.p2.director -repository http://eclipse-color-theme.github.com/update -installIU com.github.eclipsecolortheme.feature.feature.group
